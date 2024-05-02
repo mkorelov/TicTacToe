ebbb2cc6d72a8762452c3e846fcd3c16c862762b
Michael Korelov <146900945+mkorelov@users.noreply.github.com>
2024-05-01 17:50:00 -0700

    added JavaFX

--- src/FrontEnd/Main.java
+++ src/FrontEnd/Main.java
@@ -1,5 +1,7 @@
+package FrontEnd;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Tic-Tac-Toe");
     }
 }
