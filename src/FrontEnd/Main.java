310f670dfd5c385533961c8e0c9827d5fe4d4521
Michael Korelov <146900945+mkorelov@users.noreply.github.com>
2024-05-01 18:22:13 -0700

    initialize app

--- src/FrontEnd/Main.java
+++ src/FrontEnd/Main.java
@@ -1,6 +1,14 @@
 package FrontEnd;
 
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        
+    }
     public static void main(String[] args) throws Exception {
         System.out.println("Tic-Tac-Toe");
     }
