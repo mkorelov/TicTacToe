5c47927a28959ae8299cf1740de05c569dce8864
Michael Korelov <146900945+mkorelov@users.noreply.github.com>
2024-05-06 21:53:21 -0700

    skeleton code for game board

--- src/FrontEnd/GameBoard.java
+++ src/FrontEnd/GameBoard.java
@@ -1,4 +1,34 @@
 package FrontEnd;
 
-public class GameBoard {  
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.BorderPane;
+import javafx.geometry.Pos;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.*;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.BorderPane;
+import javafx.geometry.Insets;
+import javafx.scene.text.*;
+
+class GameBoard extends VBox {  
+    
+}
+
+class GameBoardHeader extends HBox {
+
+}
+
+class GameBoardFooter extends HBox {
+
+}
+
+class GameBoardFrame extends BorderPane {
+
 }
