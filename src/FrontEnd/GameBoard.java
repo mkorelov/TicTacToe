edd583d74c6b99c6a73464e131188d89e9c0a108
Michael Korelov <146900945+mkorelov@users.noreply.github.com>
2024-05-07 15:26:59 -0700

    game board basic implementation

--- src/FrontEnd/GameBoard.java
+++ src/FrontEnd/GameBoard.java
@@ -18,17 +18,55 @@ import javafx.geometry.Insets;
 import javafx.scene.text.*;
 
 class GameBoard extends VBox {  
-    
+    GameBoard() {
+        this.setPrefSize(800, 750);
+        this.setStyle("-fx-background-color: #F0F8FF;");
+    }
 }
 
 class GameBoardHeader extends HBox {
+    private Button pauseButton;
 
+    GameBoardHeader() {
+        this.setPrefSize(800, 50);
+        this.setStyle("-fx-background-color: #F0F8FF;");
+        pauseButton = new Button("Pause");
+        Text titleText = new Text("Tic-Tac-Toe");
+        titleText.setStyle("-fx-font-weight: bold; -fx-font-size: 20;");
+        this.getChildren().addAll(titleText, pauseButton);
+        this.setAlignment(Pos.CENTER);
+    }
 }
 
 class GameBoardFooter extends HBox {
-
+    GameBoardFooter() {
+        this.setPrefSize(800, 50);
+        this.setStyle("-fx-background-color: #F0F8FF;");
+        Text titleText = new Text("Tic-Tac-Toe");
+        titleText.setStyle("-fx-font-weight: bold; -fx-font-size: 20;");
+        this.getChildren().add(titleText);
+        this.setAlignment(Pos.CENTER);
+    }
 }
 
 class GameBoardFrame extends BorderPane {
+    private GameBoardHeader header;
+    private GameBoardFooter footer;
+    private GameBoard board;
 
+    GameBoardFrame() {
+        header = new GameBoardHeader();
+        footer = new GameBoardFooter();
+        board = new GameBoard();
+
+        this.setTop(header);
+        this.setCenter(board);
+        this.setBottom(footer);
+
+        addListeners();
+    }
+
+    public void addListeners() {
+
+    }
 }
