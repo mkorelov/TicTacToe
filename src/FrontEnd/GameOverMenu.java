ad2358db6422699857ec24a5da2fa4bfd5af91cc
Michael Korelov <146900945+mkorelov@users.noreply.github.com>
2024-05-13 20:19:32 -0700

    planned out game over menu functionality

--- src/FrontEnd/GameOverMenu.java
+++ src/FrontEnd/GameOverMenu.java
@@ -20,7 +20,8 @@ import javafx.scene.text.*;
 class GameOverMenu extends VBox{
     // based on result show different text, player 1 win , player 2 win , or draw
     // allow for rematch or return to main menu
-    GameOverMenu() {
+    GameOverMenu(/*String str*/) {
+        // based on results in GameBoard and Game, pass in string that will be shown
         this.setPrefSize(300, 50);
         this.setStyle("-fx-background-color: #F0F8FF;");
         Text text = new Text("Draw, Player 1 Wins, or Player 2 Wins");
