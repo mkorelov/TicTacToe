c3f01d9be062f945b7effc69486a5e3409774a42
Michael Korelov <146900945+mkorelov@users.noreply.github.com>
2024-05-28 20:04:45 -0700

    added file header outlines

--- src/FrontEnd/Main.java
+++ src/FrontEnd/Main.java
@@ -1,3 +1,39 @@
+///////////////////////////////////////////////////////////////////////////////
+//                   ALL STUDENTS COMPLETE THESE SECTIONS
+// Title:              (program's title)
+// Files:              (list of source files)
+// Quarter:            (course) Spring 2020
+//
+// Author:             (your name)
+// Email:              (your email address)
+// Instructor's Name:  (name of your instructor)
+//
+//////////////////// PAIR PROGRAMMERS COMPLETE THIS SECTION ///////////////////
+//
+//                  CHECK ASSIGNMENT PAGE TO see IF PAIR-PROGRAMMING IS ALLOWED
+//                  If pair programming is allowed:
+//                  1. Read PAIR-PROGRAMMING policy
+//                  2. Choose a partner wisely
+//                  3. Complete this section for each program file
+//
+// Pair Partner:        (name of your pair programming partner)
+// Email:               (email address of your programming partner)
+// Instructors's Name:  (name of your partner's instructor)
+// Lab Section:         (your partner's lab section number)
+//
+//////////////////// STUDENTS WHO GET HELP FROM OTHER THAN THEIR PARTNER //////
+//                   must fully acknowledge and credit those sources of help.
+//                   Instructors and TAs do not have to be credited here,
+//                   but roommates, relatives, strangers, etc do.
+//
+// Persons:          Identify persons by name, relationship to you, and email.
+//                   Describe in detail the the ideas and help they provided.
+//
+// Online sources:   Avoid web searches to solve your problems, but if you do
+//                   search, be sure to include Web URLs and description of
+//                   of any information you find.
+//////////////////////////// 80 columns wide //////////////////////////////////
+
 package FrontEnd;
 
 import javafx.application.Application;
--- src/FrontEnd/PauseMenu.java
+++ src/FrontEnd/PauseMenu.java
@@ -1,3 +1,29 @@
+///////////////////////////////////////////////////////////////////////////////
+//                   ALL STUDENTS COMPLETE THESE SECTIONS
+// Main Class File:    (name of main application class)
+// File:               (name of this class's file)
+// Quarter:            (course) Spring 2020
+//
+// Author:             (your name and email address)
+// Instructor's Name:  (name of your instructor)
+//
+//////////////////// PAIR PROGRAMMERS COMPLETE THIS SECTION ///////////////////
+// Pair Partner:       (name of your pair programming partner)
+// Email:              (email address of your programming partner)
+// Instructor's Name:  (name of your partner's instructor)
+//
+//////////////////// STUDENTS WHO GET HELP FROM OTHER THAN THEIR PARTNER //////
+//                   fully acknowledge and credit all sources of help,
+//                   other than Instructors and TAs.
+//
+// Persons:          Identify persons by name, relationship to you, and email.
+//                   Describe in detail the the ideas and help they provided.
+//
+// Online sources:   Avoid web searches to solve your problems, but if you do
+//                   search, be sure to include Web URLs and description of
+//                   of any information you find.
+//////////////////////////// 80 columns wide //////////////////////////////////
+
 package FrontEnd;
 
 import javafx.scene.layout.HBox;
